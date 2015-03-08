84ee08bf0ade94ccbb86ceaa104fb2f7b5faaf09
cfmeyers <cfmeyers@gmail.com>
2015-03-08 19:21:08 -0400

    refactor pp specs log in prior to download_csv tests

--- spec/requests/protected_pages_spec.rb
+++ spec/requests/protected_pages_spec.rb
@@ -3,21 +3,25 @@ require 'rails_helper'
 
 RSpec.describe "ProtectedPages" do
 
-  describe "download_csv page" do
+  it "is impossible to access protected_pages without being logged in" do
+    visit '/protected_pages/download_csv'
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+  end
 
-    it "has a title that indicates full access" do
+  describe "download_csv page when properly authenticated" do
+
+    before do
       visit new_user_session_path
       fill_in 'Email', with: 'c@cfmeyers.com'
       fill_in 'Password', with: '12345678'
       click_button 'Log in'
+    end
+
+    it "has a title that indicates full access" do
       visit '/protected_pages/download_csv'
       expect(page).to have_content("Full Access Housing CSV Data")
     end
 
-    it "is impossible to access page without being logged in" do
-      visit '/protected_pages/download_csv'
-      expect(page).to have_content("You need to sign in or sign up before continuing.")
-    end
 
   end
 
