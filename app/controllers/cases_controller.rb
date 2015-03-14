f6be2948afe78c96899d94832efe4ede1b589ebf
cfmeyers <cfmeyers@gmail.com>
2015-03-14 07:42:43 -0400

    return cases orderd by case number

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -2,7 +2,7 @@ class CasesController < ApplicationController
 
   
   def create
-    @cases = Case.where("file_date > ? AND file_date < ?", params[:start], params[:complete])
+    @cases = Case.where("file_date > ? AND file_date < ?", params[:start], params[:complete]).order('case_number')
     if user_signed_in?
       public_fields = ['case_number', 'case_number_integer', 'title', 'case_type', 
                        'case_status', 'status_date', 'file_date', 'property_address', 
@@ -10,7 +10,7 @@ class CasesController < ApplicationController
                        'plaintiff_attorney_name', 'defendants_json', 'defendants_self_represented', 
                        'docket_information','case_outcome', 'case_outcome_date']
     else
-      public_fields = ['case_number', 'case_type', 'case_status', 
+      public_fields = ['case_number', 'case_number_integer', 'case_type', 'case_status', 
                        'status_date', 'file_date', 'plaintiff_name_original', 
                        'plaintiff_name_guess', 'plaintiff_attorney_name', 
                        'defendants_self_represented', 'case_outcome', 'case_outcome_date']
